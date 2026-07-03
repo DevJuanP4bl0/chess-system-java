d060a71b120e5ab3c3c80aba2b9e3e486e755ee7
Juan Pablo <juanpablo140204@outlook.com>
2026-07-03 00:50:56 -0300

    First class - Position

--- src/Program/application.java
+++ src/Program/application.java
@@ -1,10 +1,14 @@
 package Program;
 
+import boardgame.Position;
+
 public class application {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, world!");
+		Position position = new Position(2, 3);
+		
+		System.out.println(position);
 	}
 
 }
